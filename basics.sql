ecc13150cbcda42023e5f325994769f8777bce5b
Steveen <steveen.echeverri@gmail.com>
2021-11-02 23:49:34 -0500

    Created basic sql and sakila DB

---
+++ basics.sql
@@ -0,0 +1,26 @@
+-- Mostrar base de datos creadas
+SHOW databases;
+
+-- Eliminar base de datos
+
+DROP database [nombre];
+
+-- Crear base de datos
+
+CREATE database [nombre];
+
+-- Ejemplo de crear una base 
+
+CREATE database practice_one;
+
+use practice_one;
+
+CREATE TABLE usuario(
+    id_usuario int(10),
+    name_usu varchar(25),
+    edad_usu int(2)
+)
+
+SHOW database;
+
+describe usuario;
---
+++ select.sql
@@ -0,0 +1,8 @@
+-- Base de datos sakila
+-- Seleccionar todos los campos de la tabla actor
+
+SELECT * FROM sakila.actor;
+
+-- actor_id, first_name, last_name, last_update
+
+SELECT first_name FROM	sakila.actor;
