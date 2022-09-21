df4a989337ec5bb681423c2ebe4ae3ea062d9635
Styvensoft <steveen.echeverri@gmail.com>
2022-09-20 23:39:25 -0500

    Created new sql WHERE

--- order_by.sql
+++ order_by.sql
@@ -7,3 +7,21 @@ SELECT * FROM sakila.country ORDER BY country DESC;
 SELECT * FROM sakila.country ORDER BY country ASC;
 
 SELECT * FROM sakila.customer ORDER BY first_name ASC;
+
+-- WHERE
+
+SELECT * FROM sakila.actor WHERE first_name = 'DAN';
+
+SELECT * FROM sakila.city WHERE city = 'Londo';
+
+SELECT * FROM sakila.city WHERE country_id = 102;
+
+SELECT * FROM sakila.customer WHERE store_id = 1;
+
+SELECT * FROM sakila.inventory WHERE film_id > 50;
+
+SELECT DISTINCT amount FROM sakila.payment WHERE amount < 3;
+
+-- diferente <>
+
+SELECT * FROM sakila.staff WHERE staff_id <> 2
